edd9a941f5d3ed273e8b9180d37c87b8a407d0fe
ShaneDelaRama <shane622005@gmail.com>
2026-04-30 23:46:42 +0800

    roughhh logic to be changed

---
+++ src/main/java/pomodoro/core/PomodoroTimer.java
@@ -0,0 +1,73 @@
+
+/* pomodoro qualities:
+- can set time for 15-50 minutes of work time
+- thinking if adjustable din yung break timer, wag nlnang pala, adittional 5 minutes if dagdag din 5 minutes sa 15 minutes (so like 15 minutes 5 rest, 20 mins 10 mins rest??)     
+- returns to work timer after break timer
+- you can put how many sets you want the pomodoro timer to repeat
+- yeah okay subject to change pa din :P
+ */
+//logic where they can set the minutes for their work time tapos automatic na yung breaktime, no ability to add more longer BreakTime
+import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
+
+
+
+      public class SetPomodoro {
+
+        private static Timer timer = new Timer();
+        private static int timeLeft
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    
+
+    public static void 
+
+} 
+
+public int getWorkTime() {
+        return WorkTime;
+    }
+
+    public int getBreakTime() {
+        return BreakTime;
+    }
+
+    public int getPomodoroNum() {
+        return PomodoroNum;
+    }
+
+    public class Timer {
+
+    }
